ff4f13918bc09e0b86a18d5b8ad873bc9ee66b0d
Dinesh <manoredinesh66@gmail.com>
2026-03-26 16:32:22 +0530

    final ecommerce

--- frontend/src/components/category/CategoryGrid.jsx
+++ frontend/src/components/category/CategoryGrid.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 export default function CategoryGrid({ categories }) {
   const categoryImages = {
   electronics: 'https://images.unsplash.com/photo-1498049794561-7780e7231661?w=400',
-  clothing: 'https://images.unsplash.com/photo-1445205170230-053b83016050?w=400',
-  homeappliances: 'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?w=400',
-  accessories: 'https://images.unsplash.com/photo-1512496015851-a90fb38ba796?w=400',
+  fashion: 'https://images.unsplash.com/photo-1445205170230-053b83016050?w=400',
+  homekitchen: 'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?w=400',
+  beauty: 'https://images.unsplash.com/photo-1512496015851-a90fb38ba796?w=400',
   books: 'https://images.unsplash.com/photo-1495446815901-a7297e633e8d?w=400',
+  sports: 'https://c4.wallpaperflare.com/wallpaper/971/967/737/sports-images-for-desktop-background-wallpaper-preview.jpg'
 };
 
   return (
