b0d84487dbe332dd51d18af376c981618e34d257
Dinesh <manoredinesh66@gmail.com>
2026-03-30 00:39:42 +0530

    fix: build success

--- backend/src/main/java/com/ecommerce/controller/TestController.java
+++ backend/src/main/java/com/ecommerce/controller/TestController.java
@@ -2,6 +2,7 @@ package com.ecommerce.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api")
